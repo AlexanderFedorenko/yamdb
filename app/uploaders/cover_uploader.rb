b2e25b834243a827120e5ffe2d716edb35a450c0
Alexander Fedorenko <alexander@fedorenko.im>
2015-12-17 21:24:53 +0200

    Add Movie cover placeholder

--- app/uploaders/cover_uploader.rb
+++ app/uploaders/cover_uploader.rb
@@ -17,12 +17,13 @@ class CoverUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+    # For Rails 3.1+ asset pipeline compatibility:
+    # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+
+    #"/images/" + [version_name, "cover-placeholder.png"].compact.join('_')
+    ActionController::Base.helpers.asset_path([version_name, "cover_placeholder.png"].compact.join('_'))
+  end
 
   process resize_to_limit: [1000, 1000]
   process convert: 'png'
